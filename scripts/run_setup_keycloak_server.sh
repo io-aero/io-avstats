defff9afe5b0eed9a37af2572dccf4beae42e676
Walter Weinmann <walter.weinmann@gmail.com>
2023-03-15 10:00:15 +0100

    23.03.08

--- scripts/run_setup_keycloak_server.sh
+++ scripts/run_setup_keycloak_server.sh
@@ -43,41 +43,24 @@ start=$(date +%s)
 echo "--------------------------------------------------------------------------------"
 echo "Docker create ${IO_AVSTATS_KEYCLOAK_CONTAINER_NAME} (Keycloak ${IO_AVSTATS_KEYCLOAK_VERSION})"
 
-export KC_HTTP_ENABLED=true
-export KC_HTTP_HOST=0.0.0.0
-export KC_HTTPS_CLIENT_AUTH=none
-export KC_HOSTNAME_STRICT=false
-export KC_HOSTNAME_URL=http://auth.io-aero.com:8080/
-export KC_DB=postgres
-export KC_DB_URL_HOST="${IO_AVSTATS_POSTGRES_KEYCLOAK_CONTAINER_NAME}"
-export KC_DB_URL_PORT="${IO_AVSTATS_POSTGRES_KEYCLOAK_CONNECTION_PORT}"
-export KC_DB_URL_DATABASE="${IO_AVSTATS_POSTGRES_KEYCLOAK_DBNAME_ADMIN}"
-export KC_DB_USERNAME="${IO_AVSTATS_POSTGRES_KEYCLOAK_USER_ADMIN}"
-export KC_DB_PASSWORD="${IO_AVSTATS_POSTGRES_KEYCLOAK_PASSWORD_ADMIN}"
-export KC_PROXY=edge
-
-echo "================================================================================"
-echo "KC_HTTP_ENABLED                   : ${KC_HTTP_ENABLED}"
-echo "KC_HTTP_HOST                      : ${KC_HTTP_HOST}"
-echo "KC_HTTPS_CLIENT_AUTH              : ${KC_HTTPS_CLIENT_AUTH}"
-echo "KC_HOSTNAME_STRICT                : ${KC_HOSTNAME_STRICT}"
-echo "KC_HOSTNAME_URL                   : ${KC_HOSTNAME_URL}"
-echo "KC_DB                             : ${KC_DB}"
-echo "KC_DB_URL_HOST                    : ${KC_DB_URL_HOST}"
-echo "KC_DB_URL_PORT                    : ${KC_DB_URL_PORT}"
-echo "KC_DB_URL_DATABASE                : ${KC_DB_URL_DATABASE}"
-echo "KC_DB_USERNAME                    : ${KC_DB_USERNAME}"
-echo "KC_DB_PASSWORD                    : ${KC_DB_PASSWORD}"
-echo "KC_PROXY                          : ${KC_PROXY}"
-echo "================================================================================"
-
-docker create -e        KEYCLOAK_ADMIN="${IO_AVSTATS_KEYCLOAK_USER_ADMIN}" \
-              -e        KEYCLOAK_ADMIN_PASSWORD="${IO_AVSTATS_KEYCLOAK_PASSWORD_ADMIN}" \
-              --name    "${IO_AVSTATS_KEYCLOAK_CONTAINER_NAME}" \
-              -p        "${IO_AVSTATS_KEYCLOAK_CONNECTION_PORT}":"${IO_AVSTATS_KEYCLOAK_CONTAINER_PORT}" \
-              --restart always \
-              quay.io/keycloak/keycloak:"${IO_AVSTATS_KEYCLOAK_VERSION}" \
-              start-dev
+docker create -e        KC_HTTP_ENABLED=true ^
+              -e        KC_HTTP_HOST=0.0.0.0 ^
+              -e        KC_HTTPS_CLIENT_AUTH=none ^
+              -e        KC_HOSTNAME_STRICT=false ^
+              -e        KC_HOSTNAME_URL=http://auth.io-aero.com:8080/ ^
+              -e        KEYCLOAK_ADMIN="${IO_AVSTATS_KEYCLOAK_USER_ADMIN}" ^
+              -e        KEYCLOAK_ADMIN_PASSWORD="${IO_AVSTATS_KEYCLOAK_PASSWORD_ADMIN}" ^
+              -e        KC_DB=postgres ^
+              -e        KC_DB_URL_HOST="${IO_AVSTATS_POSTGRES_KEYCLOAK_CONTAINER_NAME}" ^
+              -e        KC_DB_URL_PORT="${IO_AVSTATS_POSTGRES_KEYCLOAK_CONNECTION_PORT}" ^
+              -e        KC_DB_URL_DATABASE="${IO_AVSTATS_POSTGRES_KEYCLOAK_DBNAME_ADMIN}" ^
+              -e        KC_DB_USERNAME="${IO_AVSTATS_POSTGRES_KEYCLOAK_USER_ADMIN}" ^
+              -e        KC_DB_PASSWORD="${IO_AVSTATS_POSTGRES_KEYCLOAK_PASSWORD_ADMIN}" ^
+              -e        KC_PROXY=edge ^
+              --name    "${IO_AVSTATS_KEYCLOAK_CONTAINER_NAME}" ^
+              --restart always ^
+              quay.io/keycloak/keycloak:"${IO_AVSTATS_KEYCLOAK_VERSION}" ^
+              start
 
 echo "Docker start ${IO_AVSTATS_KEYCLOAK_CONTAINER_NAME} (Keycloak ${IO_AVSTATS_KEYCLOAK_VERSION}) ..."
 if ! docker start "${IO_AVSTATS_KEYCLOAK_CONTAINER_NAME}"; then
